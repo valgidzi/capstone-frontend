6b2dfa2a7539c128c2018fdf7424863fb71f09e2
Valerie Gidzinski <valgidzi@gmail.com>
2019-01-05 01:22:05 -0800

    Add NewTextForm component and getScore function for post request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import NewTextForm from './NewTextForm'
 import axios from 'axios';
 import './App.css';
 
@@ -9,11 +9,12 @@ class App extends Component {
 
     this.state = {
       materials: [],
+      score: '',
     };
   }
 
   componentDidMount() {
-    const getMaterialsEndpoint = 'http://127.0.0.1:8000/materials/'
+    const getMaterialsEndpoint = 'http://localhost:8000/materials/'
 
     axios.get(getMaterialsEndpoint)
     .then((response) => {
@@ -25,7 +26,7 @@ class App extends Component {
   }
 
   getDetail = () => {
-    axios.get('http://127.0.0.1:8000/materials/2/')
+    axios.get('http://localhost:8000/materials/2/')
       .then((response) => {
         console.log(response.data);
       })
@@ -33,6 +34,18 @@ class App extends Component {
         console.log(error.message);
       });
   }
+
+  getScore = (newText) => {
+    axios.post('http://localhost:8000/texts/', newText)
+      .then((response) => {
+        console.log(response.data.score);
+        this.setState({score: response.data.score})
+      })
+      .catch((error) => {
+        this.setState({errors: error.message})
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -41,6 +54,7 @@ class App extends Component {
           onClick={this.getDetail}>
           Material Detail
         </button>
+        <NewTextForm getScoreCallback={this.getScore} />
       </div>
     );
   }
---
+++ src/NewTextForm.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './NewTextForm.css';
+
+class NewTextForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      original: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    const field = event.target.name;
+    const value = event.target.value;
+
+    const newState = {};
+    newState[field] = value;
+    this.setState(newState);
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newText = {
+      original: this.state.text,
+    };
+
+    this.props.getScoreCallback(newText);
+
+    this.setState({
+      original: '',
+    });
+
+  }
+
+  render() {
+
+    return (
+      <form
+        id="newtextform"
+        onSubmit={this.onFormSubmit}>
+
+        <label
+          htmlFor="text">
+          Enter text here.
+        </label>
+        <textarea
+          name="text"
+          form="newtextform"
+          value={this.state.text}
+          onChange={this.onInputChange}/>
+
+        <input
+          type="submit"
+          value="Get Score"/>
+
+      </form>
+    )
+  }
+}
+
+NewTextForm.propTypes = {
+  getScoreCallback: PropTypes.func,
+}
+
+export default NewTextForm;
