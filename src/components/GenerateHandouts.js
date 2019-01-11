c9d90b731a1df211abe33b30f0d68568c60f06e8
Valerie Gidzinski <valgidzi@gmail.com>
2019-01-11 14:15:56 -0800

    Setup state for drag and drop

--- src/components/GenerateHandouts.js
+++ src/components/GenerateHandouts.js
@@ -1,19 +1,48 @@
 import React from 'react';
 
-const GenerateHandouts = (props) => {
+class GenerateHandouts extends React.Component {
+  constructor(props) {
+    super(props);
 
+    const words = props.selections.map((select, i) => {
+      return {id: `word-${i + 1}`, content: select[0]}
+    });
+
+    const wordIds = [];
+    let i;
+    for (i=0; i < words.length; i++) {
+      wordIds.push(`word-${i + 1}`)
+    }
+
+
+    this.state = {
+      words: {words},
+      column: {
+        'column-1': {
+          id: 'column-1',
+          title: 'Words',
+          wordIds: {wordIds},
+        },
+      },
+      columnOrder: ['column-1'],
+    };
+  }
+  render() {
   return (
-    <div className="genhands">
+
+      <div className="genhands">
       <h1>Generate Handouts</h1>
       <ul>
-        <li>{props.text}</li>
-         {props.selections.map(select =>
-           <li key={select[0]}>{select[0]}: {select[1]}</li>
-        )}
+      <li>{this.props.text}</li>
+      {this.props.selections.map(select =>
+        <li key={select[0]}>{select[0]}: {select[1]}</li>
+      )}
+      <li>{this.wordIds}</li>
 
       </ul>
-    </div>
-  );
+      </div>
+    )
+  }
 };
 
 export default GenerateHandouts;
