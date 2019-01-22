287b9318419fda331b0c2400280ed1386fc9b71d
Valerie Gidzinski <valgidzi@gmail.com>
2019-01-21 23:26:48 -0800

    Rename Matching component as HandoutView, remove styled components, add css file

--- src/components/HandoutView.js
+++ src/components/HandoutView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Matching from './Matching';
+import './HandoutView.css'
+import ReactHtmlParser from 'react-html-parser';
 
 class HandoutView extends Component {
   constructor(props) {
@@ -11,10 +12,41 @@ class HandoutView extends Component {
   }
 
   render() {
+
+    const html = this.props.data.text;
+
+    const words = this.props.data.words.split(',').map((word, i) => {
+      return <p key={i}>{word}</p>
+    });
+
+    const defs = this.props.data.definitions
+
+    const parsedDefString = defs.slice(0, defs.length - 1)
+
+    const goodDefs = [];
+
+    const trimmedDefs = parsedDefString.split('@').forEach((el) => {
+      let def = el[0] === "," ? el.slice(1) : el
+      goodDefs.push(def)
+    })
+
+    const definitions = goodDefs.map((def, i) => {
+      return <p key={i}>{def}</p>
+    });
+
     return(
-      <div>
-        <h3>Handout View</h3>
-          <Matching data={this.props.data}/>
+      <div className='handout-view-container'>
+        <h3>{this.props.data.title}</h3>
+          <div>{ ReactHtmlParser(html)}</div>
+          <p>Match the words with the correct definitions.</p>
+          <div className='columns-container'>
+            <div className='column'>
+              {words}
+            </div>
+            <div className='column'>
+              {definitions}
+            </div>
+          </div>
       </div>
     )
   }
