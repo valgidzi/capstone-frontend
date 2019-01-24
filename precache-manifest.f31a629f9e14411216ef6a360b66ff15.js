a82414ca4c93065c2be7d8883f25f19ca57d68a4
Valerie Gidzinski <valgidzi@gmail.com>
2019-01-23 21:36:02 -0800

    Updates

---
+++ precache-manifest.f31a629f9e14411216ef6a360b66ff15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf48f0eebc619019d021",
+    "url": "/valgidzi/capstone-frontend/static/css/main.a1be999c.chunk.css"
+  },
+  {
+    "revision": "cf48f0eebc619019d021",
+    "url": "/valgidzi/capstone-frontend/static/js/main.cf48f0ee.chunk.js"
+  },
+  {
+    "revision": "428ffe5d34f469222e26",
+    "url": "/valgidzi/capstone-frontend/static/js/1.428ffe5d.chunk.js"
+  },
+  {
+    "revision": "bb9c9003c262caf0c580",
+    "url": "/valgidzi/capstone-frontend/static/js/runtime~main.bb9c9003.js"
+  },
+  {
+    "revision": "13ae12c54593ade78ab13556b1157d35",
+    "url": "/valgidzi/capstone-frontend/index.html"
+  }
+];
